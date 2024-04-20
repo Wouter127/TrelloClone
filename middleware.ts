35a05cd99b08befc19cc97634f52970af54b79b0
Wouter127 <r0785520@student.thomasmore.be>
2024-04-20 18:04:50 +0200

    6. organizations

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
- 
+import { NextResponse } from "next/server";
+
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedFromLoggedIn(req) && auth().userId) {
+    let path = '/select-org';
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
+    }
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection)
+  }
+  if (auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org") {
+    return NextResponse.redirect(new URL("/select-org", req.url));
+  }
 });
 
 const isProtectedRoute = createRouteMatcher([
   '/protected(.*)',
+  '/organization(.*)',
 ]);
+
+const isProtectedFromLoggedIn = createRouteMatcher([
+  '/'
+])
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
