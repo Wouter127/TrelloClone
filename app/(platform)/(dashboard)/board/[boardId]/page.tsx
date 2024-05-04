e639d986b42e3ac68d56344ab77609848b9d3389
Wouter127 <r0785520@student.thomasmore.be>
2024-05-04 18:59:56 +0200

    build error typo

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -36,7 +36,7 @@ const BoardIdPage = async ({params}: BoardIdPageProps) => {
 
     return (
         <div className="p-4 h-full overflow-x-auto">
-            <ListContainer boarId={params.boardId} data={lists} />
+            <ListContainer boardId={params.boardId} data={lists} />
         </div>
     )
 }
