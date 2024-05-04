f4c07a178ca7f6b07c0cbfb7a672f911da48b919
Wouter127 <r0785520@student.thomasmore.be>
2024-05-04 16:01:06 +0200

    15. Board Page

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90"
+        primary: "bg-sky-700 text-primary-foreground hover:bg-sky-700/90",
+        transparent: "bg-transparent text-white hover:bg-white/20"
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -36,7 +37,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
