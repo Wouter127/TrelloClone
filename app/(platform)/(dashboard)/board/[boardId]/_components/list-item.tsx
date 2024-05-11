a7f9d32d264977bcb3b2eea991f5ca7a2b062ed8
Wouter127 <r0785520@student.thomasmore.be>
2024-05-11 17:14:41 +0200

    20. Drag and drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -3,10 +3,10 @@
 import { ListWithCards } from "@/types";
 import ListHeader from "./list-header";
 import { ElementRef, useRef, useState } from "react";
-import { useAction } from "@/hooks/use-action";
 import CardForm from "./card-form";
 import { cn } from "@/lib/utils";
 import CardItem from "./card-item";
+import { Draggable, Droppable } from "@hello-pangea/dnd"
 
 interface ListItemProps {
     data: ListWithCards;
@@ -14,7 +14,7 @@ interface ListItemProps {
 }
 
 
-const ListItem = ({data, index}:ListItemProps) => {
+const ListItem = ({ data, index }: ListItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
 
     const textAreaRef = useRef<ElementRef<"textarea">>(null);
@@ -30,54 +30,27 @@ const ListItem = ({data, index}:ListItemProps) => {
         })
     }
 
-    // const { execute } = useAction(updateList, {
-    //     onSuccess: (data) => {
-    //         toast.success(`Renamed to ${data.title}`);
-    //         setTitle(data.title);
-    //         disableEditing();
-    //     },
-    //     onError: (error) => {
-    //         toast.error(error)
-    //     }
-    // });
-
-    // const handleSubmit = (formData: FormData) => {
-    //     const title = formData.get("title") as string;
-    //     const id = formData.get("id") as string;
-    //     const boardId = formData.get("boardId") as string;
-
-    //     if (title === data.title) {
-    //         return disableEditing();
-    //     }
-
-    //     execute({ title, id, boardId });
-
-    // }
-
-    // const onBlur = () => {
-    //     formRef.current?.requestSubmit();
-    // }
-
-    // const onKeyDown = (e: KeyboardEvent) => {
-    //     if (e.key === "Escape") {
-    //         formRef.current?.requestSubmit();
-    //     }
-    // }
-
-    // useEventListener("keydown", onKeyDown);
-
     return (
-        <li className="shrink-0 h-full w-[272px] select-none">
-            <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-                <ListHeader onAddCard={enableEditing} data={data} />
-                <ol className={cn("mx-1 px-1 py-0.5 flex flex-col gap-y-2", data.cards.length > 0 ? "mt-2" : "mt-0")}>
-                    {data.cards.map((card, index) => (
-                        <CardItem index={index} key={card.id} data={card} />
-                    ))}
-                </ol>
-                <CardForm ref={textAreaRef} isEditing={isEditing} enableEditing={enableEditing} disableEditing={disableEditing} listId={data.id} />
-            </div>
-        </li>
+        <Draggable draggableId={data.id} index={index}>
+            {(provided) => (
+                <li {...provided.draggableProps} ref={provided.innerRef} className="shrink-0 h-full w-[272px] select-none">
+                    <div {...provided.dragHandleProps} className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
+                        <ListHeader onAddCard={enableEditing} data={data} />
+                        <Droppable droppableId={data.id} type="card">
+                            {(provided) => (
+                                <ol ref={provided.innerRef} {...provided.droppableProps} className={cn("mx-1 px-1 py-0.5 flex flex-col gap-y-2", data.cards.length > 0 ? "mt-2" : "mt-0")}>
+                                    {data.cards.map((card, index) => (
+                                        <CardItem index={index} key={card.id} data={card} />
+                                    ))}
+                                    {provided.placeholder}
+                                </ol>
+                            )}
+                        </Droppable>
+                        <CardForm ref={textAreaRef} isEditing={isEditing} enableEditing={enableEditing} disableEditing={disableEditing} listId={data.id} />
+                    </div>
+                </li>
+            )}
+        </Draggable>
     );
 }
 
